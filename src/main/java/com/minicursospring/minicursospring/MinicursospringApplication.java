4364b5e08c10469ef695011a91e568719bb320f7
Lucas Moraes <lucashsmoraes@gmail.com>
2018-10-01 21:06:34 -0300

    Iniciando projeto

---
+++ src/main/java/com/minicursospring/minicursospring/MinicursospringApplication.java
@@ -0,0 +1,12 @@
+package com.minicursospring.minicursospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinicursospringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinicursospringApplication.class, args);
+    }
+}
---
+++ src/test/java/com/minicursospring/minicursospring/MinicursospringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.minicursospring.minicursospring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MinicursospringApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
