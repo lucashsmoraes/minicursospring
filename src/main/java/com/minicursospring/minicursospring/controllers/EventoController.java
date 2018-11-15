631fc05d7062366ea2e372a3f996268910eeb33f
Lucas Moraes <lucashsmoraes@gmail.com>
2018-11-15 01:46:12 -0200

    Deletando elementos do banco de dados

--- src/main/java/com/minicursospring/minicursospring/controllers/EventoController.java
+++ src/main/java/com/minicursospring/minicursospring/controllers/EventoController.java
@@ -7,10 +7,7 @@ import com.minicursospring.minicursospring.repository.EventoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -71,6 +68,24 @@ public class EventoController {
         convidadoRepository.save(convidado);
         attributes.addFlashAttribute("mensagem", "Convidado adicionado com sucesso!");
         return "redirect:/{codigo}";
+    }
+
+    @RequestMapping("/deletarEvento")
+    public String deletarEvento(long codigo){
+        Evento evento = eventoRepository.findByCodigo(codigo);
+        eventoRepository.delete(evento);
+        return "redirect:/eventos";
+    }
+
+    @RequestMapping("/deletarConvidado")
+    public String deletarConvidado(long codigo){
+        Convidado convidado = convidadoRepository.findById(codigo);
+        convidadoRepository.delete(convidado);
+
+        Evento evento = convidado.getEvento();
+        long codigoLong = evento.getCodigo();
+        String codigoEvento = "" + codigoLong;
+        return "redirect:/" + codigoEvento ;
     }
 
 }
